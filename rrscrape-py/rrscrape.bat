db919f4bc6fe941901b30592c73d113d6868d9e3
snellers <1669156+snellers@users.noreply.github.com>
2025-05-26 19:40:21 +0100

    - Simplify scripts by using uv run

--- rrscrape-py/rrscrape.bat
+++ rrscrape-py/rrscrape.bat
@@ -1,20 +1,8 @@
 @echo off
 REM This is a launcher script for use on Windows. 
-REM It checks that the python virtual environment is configured and up to date before running the python script.
-REM Install 'uv' if it's missing
+REM It checks that uv is installed then runs the script in a python virtual environment.
 where /q uv
 if errorlevel 1 (
     winget install --id=astral-sh.uv  -e
 )
-REM Create the python virtual environment for the project if it's missing
-if not exist .venv (
-    uv venv
-    call .venv\Scripts\activate
-    uv pip install -r pyproject.toml
-    call deactivate
-)
-REM Run using the project's virtual environment
-call .venv\Scripts\activate.bat
-uv sync -q
-python rrscrape.py
-call deactivate
+uv run .\rrscrape.py
